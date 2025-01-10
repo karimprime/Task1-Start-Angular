1a6bca5e42cd38b08d2bc010143535a59cc68562
karim Ashraf <karimashraf000@gmail.com>
2025-01-10 15:19:18 +0200

    Update Done

--- src/app/nav/nav.component.ts
+++ src/app/nav/nav.component.ts
@@ -1,11 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
-  imports: [],
+  imports: [RouterLink , RouterLinkActive],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
 
+  isScroll : boolean = false;
+
+  @HostListener('window:scroll', [])
+  scrollUser(){
+    if(scrollY>0){
+      this.isScroll = true;
+    }else{
+      this.isScroll = false;
+
+    }
+  }
 }
